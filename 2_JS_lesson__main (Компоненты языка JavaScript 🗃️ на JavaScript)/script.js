88792e972a18631aa87da1c49697bc89432464b0
RomaniDolika <romanicargo@icloud.com>
2026-02-03 03:32:09 +0300

    first commit

---
+++ 2_JS_lesson__main (Компоненты языка JavaScript 🗃️ на JavaScript)/script.js
@@ -0,0 +1,31 @@
+const message = "Привет, Мир!";
+document.getElementById("output").innerHTML = message;
+//
+const sum = 10 + 15
+console.log(sum);
+//
+const result = (10+5)*2;
+console.log(result);
+
+//
+const userName = 'John Doe';
+const userAge = 25;
+
+function greetUser(name, age) {
+    return `Hello, ${name}. You are ${age} years old.`;
+}
+
+console.log(greetUser(userName, userAge));
+
+//
+const name = 'John Doe';
+console.log(name);
+//
+let name2 = 'John';
+name2 = 'John';
+console.log(name2);
+//
+let someValue = 10;
+someValue +=30;// someValue = someValue +30
+console.log(someValue);
+//
---
+++ 3_JS_lesson__main (Переменные JavaScript 🗃️ на JavaScript)/script.js
@@ -0,0 +1,30 @@
+//let
+let price = 100;
+
+console.log(price);
+
+price = 150;
+
+console.log(price);
+
+//const
+const price = 100;
+
+console.log(price);
+
+price = 150;
+
+console.log(price);
+
+/*
+Const это не изменная переменная
+
+Let это переопределяемая переменная
+ */
+
+
+
+
+
+
+
